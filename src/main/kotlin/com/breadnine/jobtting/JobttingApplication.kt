fc511126bdde596084b6b94798557dbbbb0ff8e6
김영재 <ozh0914@gmail.com>
2020-01-27 23:10:22 +0900

    Initial commit

---
+++ src/main/kotlin/com/breadnine/jobtting/JobttingApplication.kt
@@ -0,0 +1,11 @@
+package com.breadnine.jobtting
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JobttingApplication
+
+fun main(args: Array<String>) {
+    runApplication<JobttingApplication>(*args)
+}
